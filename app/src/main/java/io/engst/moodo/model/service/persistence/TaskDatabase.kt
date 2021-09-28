76f3733d1388e7fe78a57ef2c59c762214c8772f
been6171 <benjamin.engst@esolutions.de>
2021-09-28 22:43:05 +0200

    add backup and restore for sqlite db

--- app/src/main/java/io/engst/moodo/model/service/persistence/TaskDatabase.kt
+++ app/src/main/java/io/engst/moodo/model/service/persistence/TaskDatabase.kt
@@ -1,10 +1,23 @@
 package io.engst.moodo.model.service.persistence
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.util.Log
+import android.widget.Toast
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import io.engst.moodo.ui.MainActivity
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
+const val TAG = "TaskDatabase"
 
 @Database(entities = [TaskEntity::class], version = 1)
 @TypeConverters(TaskConverter::class)
@@ -12,19 +25,98 @@ abstract class TaskDatabase : RoomDatabase() {
     abstract val taskDao: TaskDao
 
     companion object {
-        private var instance: TaskDatabase? = null
+        private var database: TaskDatabase? = null
 
         fun getInstance(context: Context): TaskDatabase {
-            if (instance == null) {
+            if (database == null) {
                 synchronized(TaskDatabase::class.java) {
-                    instance = Room.databaseBuilder(
+                    database = Room.databaseBuilder(
                         context.applicationContext,
                         TaskDatabase::class.java,
                         "moodoDb"
                     ).build()
                 }
             }
-            return instance!!
+            return database!!
+        }
+
+        fun backup(context: Context) {
+            if (database == null) {
+                Log.e(TAG, "database not initialized")
+                return
+            }
+
+            val dbName = database!!.openHelper.databaseName
+            val backupPath = context.filesDir
+            //val backupPath = context.getExternalFilesDir("backup")
+            val databaseFile = context.getDatabasePath(dbName)
+
+            Log.d(TAG, "Backup database $dbName at $databaseFile")
+
+            database!!.close()
+
+            val fileName = "$dbName-${
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"))
+            }.sqlite3"
+
+            databaseFile.copyTo(File("$backupPath/$fileName"))
+
+            Log.d(TAG, "Saved $fileName to $backupPath")
+            Toast.makeText(context, "Backup successfully", Toast.LENGTH_LONG)
+                .show()
+            restartApp(context)
+        }
+
+        fun showRestoreDialog(context: Context) {
+            val backupPath = context.filesDir
+            val backupFiles = backupPath.listFiles()
+
+            if (backupFiles.isNullOrEmpty()) {
+                Log.w(TAG, "No backups available at $backupPath")
+                Toast.makeText(context, "No backups found", Toast.LENGTH_LONG).show()
+                return
+            }
+
+            val backupFileArray = backupFiles.map { it.name }.toTypedArray()
+
+            MaterialAlertDialogBuilder(context)
+                .setTitle("Restore database")
+                .setItems(backupFileArray) { _, which ->
+                    val fileName = backupFileArray[which]
+                    val file = File("$backupPath/$fileName")
+                    restore(context, file)
+                }
+                .setOnCancelListener {
+                    Log.d(TAG, "Restore dialog canceled")
+                }
+                .show()
+        }
+
+        private fun restore(context: Context, file: File) {
+            Log.d(TAG, "Restore database from $file")
+
+            val dbName = database!!.openHelper.databaseName
+            val databaseFile = context.getDatabasePath(dbName)
+            database!!.close()
+
+
+            if (file.extension == "sqlite3") {
+                file.copyTo(databaseFile, overwrite = true)
+                Log.d(TAG, "Restored database from $file")
+                restartApp(context)
+            } else {
+                Log.e(TAG, "File is no SQLite database: $file")
+            }
+        }
+
+        private fun restartApp(context: Context) {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(intent)
+            if (context is Activity) {
+                context.finish()
+            }
+            Runtime.getRuntime().exit(0)
         }
     }
 }
--- app/src/main/java/io/engst/moodo/ui/MainActivity.kt
+++ app/src/main/java/io/engst/moodo/ui/MainActivity.kt
@@ -3,11 +3,34 @@ package io.engst.moodo.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.engst.moodo.R
+import io.engst.moodo.model.service.persistence.TaskDatabase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val topAppBar =
+            findViewById<com.google.android.material.appbar.MaterialToolbar>(R.id.topAppBar)
+        topAppBar.setNavigationOnClickListener {
+            // Handle navigation icon press
+        }
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.backup -> {
+                    TaskDatabase.backup(applicationContext)
+                    true
+                }
+                R.id.restore -> {
+                    TaskDatabase.showRestoreDialog(this)
+                    true
+                }
+                R.id.about -> {
+                    false
+                }
+                else -> false
+            }
+        }
     }
 }
